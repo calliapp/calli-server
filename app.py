6e52e1359e54fc1f448732d69c464047f7a0b0c1
ash <thomasbass2001@gmail.com>
2020-07-12 20:55:52 +0100

    fixes offset

--- app.py
+++ app.py
@@ -157,7 +157,7 @@ def dash_create():
         else:
             duration = 60
 
-        start = ((dt.datetime.combine(date.date(), time.time()).replace(tzinfo=dt.timezone.utc)) + dt.timedelta(hours=int(-1*session['offset'])))
+        start = ((dt.datetime.combine(date.date(), time.time()).replace(tzinfo=dt.timezone.utc)) + dt.timedelta(hours=(-1*int(session['offset']))))
         event['start'] = start.strftime("%s")
 
         if request.form.get('type') == "event":
